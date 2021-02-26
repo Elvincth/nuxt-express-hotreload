a95b78d52af1b2b66fcdc66951424d044088079c
Elvin Chu <elvincth@gmail.com>
2021-02-26 15:40:10 +0800

    To test

--- server/test.ts
+++ server/test.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 const router = Router();
 
 router.get('/', (_req: Request, res: Response) => {
-  return res.status(200).send('Tfrfrest!');
+  return res.status(200).send('Test!');
 });
 
 export default router;
